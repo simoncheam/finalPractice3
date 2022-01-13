42a953206662022cefeb2ee01f8d133bcddfe4ec
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-01-13 17:53:05 -0500

    private wrapper update

--- src/client/components/PrivateWrapper.tsx
+++ src/client/components/PrivateWrapper.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Outlet } from 'react-router'
+import { Navigate, Outlet } from 'react-router'
 import { APIService } from '../services/APIService'
 
 const PrivateWrapper = ({ children }: PrivateRouteProps) => {
 
-    const [isAuthed, setIsAuthed] = useState(false)
-    const [isLoaded, setIsLoaded] = useState<boolean>(false)
-
+    const [isAuthed, setIsAuthed] = useState(false);
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
 
     useEffect(() => {
@@ -17,30 +16,39 @@ const PrivateWrapper = ({ children }: PrivateRouteProps) => {
                 const tokenStatus = res.message === 'valid';
                 console.log({ tokenStatus });
                 setIsAuthed(tokenStatus)
+                setIsLoaded(true)
 
             })
             .catch(e => {
                 console.log(e)
+                setIsLoaded(true)
                 setIsAuthed(false)
             })
 
     }, [])
 
 
+    if (!isLoaded) { return <> Loading...</> }
+
+    if (!isAuthed) {
+        return <Navigate to='/login' />
+    } else {
+
+        return (
+            <div>
+                <h1 className="text-primary text-center">Private Wrapper </h1>
+
+                {children}
+
+                <Outlet />
 
 
+            </div>
+        )
 
-    return (
-        <div>
-            <h1 className="text-primary text-center">Private Wrapper </h1>
-
-            {children}
-
-            <Outlet />
+    }
 
 
-        </div>
-    )
 }
 
 interface PrivateRouteProps {
